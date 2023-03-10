a7c5d10e5127fe8506f997ae0b7a9c03811cd8bf
camilo gonzalez <camilogonzalez.nttdata@latam.com>
2023-03-10 15:00:38 -0300

    avance del juego del gato

---
+++ src/App.jsx
@@ -0,0 +1,102 @@
+
+import './App.css'
+import { useState } from 'react'
+
+const TURNS = {
+  X: 'x',
+  O: 'o',
+} 
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+    updateBoard(index)
+  }
+  return (
+    <div  onClick={handleClick} className={className}>
+      {children}
+    </div>
+  )
+}
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+function App() {
+
+  const [board, setBoard] = useState(Array(9).fill(null))
+ 
+
+  const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null) // nul no hay ganador, false empate
+  const checkWinner = (boardToCheck) => {
+    // revisar todas las combinaciones ganadoras para ver si x u O gano
+    for(const combo of WINNER_COMBOS){
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] && // miro si en el cero hay una X o una O
+        boardToCheck[a] === boardToCheck[b] && // si en el 
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
+    }
+    return null
+  }
+
+  const updateBoard = (index) => {
+    // si ya tiene algo
+    if(board[index] || winner) return
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+    // revisar si tenemos ganador
+    const newWinner = checkWinner(newBoard)
+    if(newWinner){
+      setWinner(newWinner)
+      alert(`El ganador es ${newWinner}`)
+    }
+  }
+
+
+  return (
+    <main className='board'>
+      <h1>El juego del gato</h1>
+      <section className="game">
+        {
+          board.map((_, index) => {
+            return (
+              <Square 
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+                >
+               {board[index]}
+              </Square>
+            )
+          })
+        }
+      </section>
+      <section>
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+    </main>
+  )
+}
+
+export default App
